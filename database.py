0263a705f196769bb44c0ace61ae05884c22cc78
dropex7 <32023430+dropex7@users.noreply.github.com>
2020-06-03 11:26:54 +0300

    Update database.py

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ import pymongo
 class DB:
     def __init__(self):
         self.client = pymongo.MongoClient(
-            "mongodb+srv://dropex777:oM6JF2dJvSPs8r0B@cluster0-iipk4.mongodb.net/test?retryWrites=true&w=majority")
+            "mongodb+srv://dropex777:<password>@cluster0-iipk4.mongodb.net/test?retryWrites=true&w=majority")
 
     def addPost(self, id, request):
         db = self.client.users
